9c64a0699509c3a1ebc9b9054bdc7dff251da493
Sampada-VS <samssv1@gmail.com>
2020-11-02 22:35:03 +0530

    Adding- find maximum integer

--- src/main/java/com/bridgelabz/maxgenerics/FindMaximum.java
+++ src/main/java/com/bridgelabz/maxgenerics/FindMaximum.java
@@ -1,7 +1,18 @@
 package com.bridgelabz.maxgenerics;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class FindMaximum {
 	public static void main(String[] args) {
-		System.out.println("Welcome to find maximum problem.");
+		List<Integer> intArray = Arrays.asList(13,4,8);
+		System.out.println("Max Integer :"+Collections.max(intArray, new MaxInteger()));
+		
 	}
+}
+class MaxInteger implements Comparator<Integer> {
+	public int compare (Integer o1,Integer o2) {
+		return o1.compareTo(o2);	}
 }

1249eee1063d7c1ff7dc239512539e339f43269d
Sampada-VS <samssv1@gmail.com>
2020-11-03 19:26:17 +0530

    Adding-print method to print maximim using generic method

--- src/main/java/com/bridgelabz/maxgenerics/FindMaximum.java
+++ src/main/java/com/bridgelabz/maxgenerics/FindMaximum.java
@@ -4,28 +4,18 @@ import java.util.Comparator;
 import java.util.List;
 
 public class FindMaximum {
-/*	T x,y,z;
-	public FindMaximum (List <T> Array) {
-		this.x=x;
-		this.y=y;
-		this.z=z;
 
-	}
-	public T maximum() {
-		return FindMaximum.maximum(x,y,z);
-	}*/
 	public static <T extends Comparable<T>> T maximum (List <T> array) {
-/*		T max=x;
-		if(y.compareTo(max) > 0)
-			max=y;
-		if(z.compareTo(max) > 0)
-			max=z;
-		return max;
-*/
+
 		T findMax = array.stream().sorted(Comparator.<T>reverseOrder())
                 .findFirst()
                 .get();
+		printMax(array,findMax);
 		return findMax;
+	}
+
+	public static <T> void printMax (List <T> array,T findMax) {
+		System.out.println("Max of"+array+" is :"+findMax);
 	}
 		
 }
